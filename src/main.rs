1cdfd5925e7d7042fbb2200cb6c3ea2e855aa1b6
PhilipEHausner <Philip.E.Hausner@gmail.com>
2024-02-17 23:45:04 +0100

    Add clap for command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+#[derive(clap::Parser, Debug)]
+#[command(version, about = "A statically typed lox interpreter.")]
+struct Args {
+    /// The input file to parse and execute
+    #[arg(short, long)]
+    file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Hello World!");
 }
